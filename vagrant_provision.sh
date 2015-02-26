17de99edae27be852361fd2f65146fe03ad24399
Carlus Henry <carlushenry@gmail.com>
2015-02-25 21:30:50 -0500

    finally got a working image with the right versions

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 
+echo "*********************"
 echo "Self update apt-get"
+echo "*********************"
 apt-get update
 
+echo "*********************"
 echo "Installing Git"
+echo "*********************"
 apt-get install -y git
 
-echo "Updating to npm 2.6.0"
-npm install -g npm@2.6.0
+echo "*********************"
+echo "Installing Curl"
+echo "*********************"
+apt-get install -y curl
 
+echo "*********************"
+echo "Installing Node"
+echo "*********************"
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+apt-get install -y nodejs
+apt-get install -y npm
+npm install -g npm
+apt-get install -y build-essential
+
+echo "*********************"
 echo "Updating yo bower grunt-cli"
-npm install --global yo bower grunt-cli
+echo "*********************"
+npm install --global yo 
+npm install --global bower 
+npm install --global grunt-cli
