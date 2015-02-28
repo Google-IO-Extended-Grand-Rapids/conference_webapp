7e481eec32d1c9e46c9617a9455f4c56c0c26622
Carlus Henry <carlushenry@gmail.com>
2015-02-28 06:28:54 -0500

    the conferencesList is working again

--- app/scripts/controllers/conferences.js
+++ app/scripts/controllers/conferences.js
@@ -8,14 +8,14 @@
  * Controller of the vagrantApp
  */
 angular.module('vagrantApp')
-  .controller('ConferencesCtrl', function ($scope, $http) {
-    $scope.conferenceList = [
-      {name: 'Conference 1'},
-      {name: 'Conference 2'}
-    ];
-    $scope.conferenceJson = function($http) {
-      $http.get('/scripts/conferences.json').success(response) {
-        return response.data;
-      }
-    }
-  });
+  .controller('ConferencesCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.conferenceList = [{name: "Conference1"}, {name: "Conference2"}];
+    $http.get('data/conferences.json').
+        success(function(data, status, headers, config) {
+          $scope.conferences = data;
+        }).
+        error(function(data, status, headers, config) {
+          // log error
+        });
+  }]
+);
