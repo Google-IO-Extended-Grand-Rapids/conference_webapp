33f8d7840ecdf6bc13f525cd4b5364e6225742db
Dave Schoutens <dschoutens@gmail.com>
2015-05-25 03:03:56 -0400

    Initial commit of schedule page.  Still needs responsive fixes, time-slot merging, and accordion effect added

--- partial/schedule/schedule.js
+++ partial/schedule/schedule.js
@@ -1,4 +1,24 @@
-angular.module('io2015').controller('ScheduleCtrl',function($scope){
+angular.module('io2015').controller('ScheduleCtrl', function($scope, Io2015ScheduleApi){
+    var self = this;
 
+    self.sessions = { 'day1': [], 'day2': [] };
+
+    Io2015ScheduleApi.getConferenceSessions().then(function (data) {
+        data.sort(function (a, b) {
+            return a.startDttm < b.startDttm ? -1 : 1;
+        });
+
+        // TODO: Determine which sessions' time to show
+
+        self.sessions.day1 = data.filter(function (session) {
+            return session.startDttm < new Date('05/29/2015');
+        });
+
+        self.sessions.day2 = data.filter(function (session) {
+            return session.startDttm > new Date('05/29/2015');
+        });
+    });
+
+    // TODO: Implement function shouldHideTime(sessionId)
 
 });
